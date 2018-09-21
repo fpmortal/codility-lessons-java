6498a00013dfa11b70a980a39f9135167e6fb326
fpmortal <42036825+fpmortal@users.noreply.github.com>
2018-09-21 18:07:50 +0300

    Lesson 1, BinaryGap: Solution

--- src/main/java/lesson01/BinaryGap.java
+++ src/main/java/lesson01/BinaryGap.java
@@ -3,6 +3,24 @@ package lesson01;
 public class BinaryGap {
 
     public int solution(int n) {
-        return -1;
+        String bits = Integer.toBinaryString(n);
+        int max = 0;
+        int cur = 0;
+
+        for (char bit : bits.toCharArray()) {
+            switch (bit) {
+                case '0':
+                    cur += 1;
+                    break;
+                case '1':
+                    if (cur > max) {
+                        max = cur;
+                    }
+                    cur = 0;
+                    break;
+            }
+        }
+
+        return max;
     }
 }
